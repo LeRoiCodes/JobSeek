4f42d961a0089276a519b3f33f8964df816bc398
Brandon Armand Nyamkimbi <111974934+LeRoiCodes@users.noreply.github.com>
2024-05-06 16:37:06 +0100

    setting up app structure and setting up express

---
+++ Backend/app.js
@@ -0,0 +1,44 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import fileUpload from "express-fileupload";
+import userRouter from "./routes/userRoutes";
+import applicationRouter from "./routes/applicationRouter";
+import jobRouter from "./routes/jobRouter";
+import { dbConnection } from "./database/dbConnection";
+
+
+const app = express()
+
+//setting up environment variables
+dotenv.config({path:"./config/config.env"})
+
+//setting up cors
+app.use(cors({
+    origin: [process.env.FRONTEND_URL],
+    methods: ["GET", "POST", "DELETE", "PUT"],
+    credentials: true
+}))
+
+//express settings
+app.use(cookieParser())
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+//settings for uploading files
+app.use(fileUpload({
+    useTempfiles: true,
+    tempfileDir:"/temp/"
+}))
+
+//routers
+app.use("/api/v1/user", userRouter)
+app.use("api/v1/application", applicationRouter)
+app.use("api/v1/job", jobRouter)
+
+//calling function to connect to database
+dbConnection()
+
+
+export default app;
