03730a9185c560add2754e40f5fcf5d1eef51c40
Brandon Armand Nyamkimbi <111974934+LeRoiCodes@users.noreply.github.com>
2024-05-10 22:24:03 +0100

    creating register route

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -1,4 +1,34 @@
+import { catchAsyncError } from "../middlewares/catchAsyncError.js"
+import ErrorHandler from "../middlewares/error.js";
+import User from "../models/userModel.js";
 
 
 
-export const 
+export const register = catchAsyncError(async(req, res, next) => {
+    const {name, phone, email, role, password} = req.body;
+    if(!name || !phone || email || role || !password
+    ){
+        return next(new ErrorHandler("Please fill full registration form"))
+    }
+    
+    const isEmail = await User.findOne({email})
+    if(isEmail){
+        return next(new ErrorHandler("Email already exists!!"))
+    }
+    const user = await User.create({
+        name,
+        email,
+        phone,
+        role,
+        password
+    })
+    res.status(200).json({
+        success: true,
+        message: "User Registered",
+        user,
+    })
+})
+
+export const login = () => {
+
+}
--- Backend/middlewares/auth.js
+++ Backend/middlewares/auth.js
@@ -1,4 +1,18 @@
-import { catchAsyncError } from "./catchAsyncError";
+import User from "../models/userModel.js";
+import { catchAsyncError } from "./catchAsyncError.js";
+import ErrorHandler from "./error.js"
+import jwt from "jsonwebtoken"
 
+export const isAuthorized = catchAsyncError(async (req, res, next) => {
+    //get token from cookies
+    const {token} = req.cookies;
+    if(!token) {
+        return next(new ErrorHandler("User not authoried", 400))
+    }
+    //decode token to get id of user
+    const decoded  = jwt.verify(token, process.env.JWT_SECRET_KEY);
 
-export const isAuth
+    req.user = await User.findById(decoded.id)
+
+    next()
+})
