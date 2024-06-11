e37c00cc38180dc7f80b012d4b230c0f101d570f
Brandon Armand Nyamkimbi <111974934+LeRoiCodes@users.noreply.github.com>
2024-06-11 15:16:13 +0100

    created controllers and routes for application
    postApplication
    employerGetAllApplication
    JobseekerGetAllApplication
    deleteApplication

--- Backend/routes/applicationRouter.js
+++ Backend/routes/applicationRouter.js
@@ -1,9 +1,15 @@
 import express from "express";
-
+import { employerGetAllApplication, jobSeekerDeleteApplication, jobSeekerGetAllApplication, postApplication } from "../controllers/applicationController";
+import { isAuthorized } from "../middlewares/auth";
 
 
 
 
 const router = express.Router()
+
+router.get("/jobseeker/getall", isAuthorized, jobSeekerGetAllApplication)
+router.get("/employer/getall", isAuthorized, employerGetAllApplication)
+router.delete("/delete/:id", isAuthorized, jobSeekerDeleteApplication)
+router.post("/post", isAuthorized, postApplication)
 
 export default router
