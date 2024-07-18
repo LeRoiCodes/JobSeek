619f63cf901a652b26481c7a80b8ab334f9a0002
Brandon Armand Nyamkimbi <111974934+LeRoiCodes@users.noreply.github.com>
2024-07-18 14:41:03 +0100

    added JobDetails and PostJob pages

--- Backend/routes/jobRouter.js
+++ Backend/routes/jobRouter.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { deleteJob, getAllJobs, getmyJobs, postJob, updateJob } from "../controllers/jobController.js";
+import { deleteJob, getAllJobs, getmyJobs, getSingleJob, postJob, updateJob } from "../controllers/jobController.js";
 import {isAuthorized} from "../middlewares/auth.js"
 
 
@@ -9,8 +9,9 @@ const router = express.Router()
 router.get("/getall", getAllJobs)
 router.post("/post", isAuthorized, postJob)
 router.get("/getmyjobs", isAuthorized, getmyJobs)
-router.post("updatejob/:id", isAuthorized, updateJob)
+router.put("updatejob/:id", isAuthorized, updateJob)
 router.delete("delete/:id", isAuthorized, deleteJob)
+router.get("/:id", isAuthorized, getSingleJob)
 
 
 export default router
